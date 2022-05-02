dc1346b044c03297fccd8cb962d3caf300e545a9
Darya <allexxika@yandex.ru>
2022-05-02 21:13:21 +0300

    Первый коммит

---
+++ src/ru/skypro/Employee.java
@@ -0,0 +1,42 @@
+package ru.skypro;
+
+public class Employee {
+    private String firstName;
+    private String secondName;
+    private String thirdName;
+    private int depart;
+    private double salary;
+    private int id = 1;
+
+    public Employee(String firstName, String secondName, String thirdName, int depart, double salary) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.thirdName = thirdName;
+        this.depart = depart;
+        this.salary = salary;
+        id++;
+
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getSecondName() {
+        return this.secondName;
+    }
+    public String getThirdName() {
+        return this.thirdName;
+    }
+    public int getDepart() {
+        return this.depart;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public String toString() {
+        return "ФИО  " + this.firstName + " " + this.secondName + " " + this.thirdName + ",  отдел " + this.depart + ",  зартплата " + this.salary;
+    }
+
+}
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,56 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Employee[] member = new Employee[10];
+        member[0] = new Employee("Титова", "Диана", "Сергеевна", 1, 230000);
+        member[1] = new Employee("Иванов", "Михаил", "Викторович", 2, 250000);
+        member[2] = new Employee("Нечаева", "Лилия", "Кирилловна", 3, 215000);
+        member[3] = new Employee("Грачев", "Денис", "Матвеевич", 4, 145000);
+        member[4] = new Employee("Григорьев", "Матвей", "Давидович", 5, 185000);
+        member[5] = new Employee("Пугачев", "Владислав", "Эминович", 1, 198000);
+        member[6] = new Employee("Тихонов", "Андрей", "Давидович", 2, 220000);
+        member[7] = new Employee("Дубровина", "Ольга", "Михайловна", 3, 300000);
+        member[8] = new Employee("Королев", "Владимир", "Леонович", 4, 280000);
+        member[9] = new Employee("Андреева", "Варвара", "Александровна", 5, 260000);
+
+        for (int i = 0; i < member.length; i++) {
+            System.out.println(member[i]);
+        }
+        System.out.println();
+
+        double summ = 0;
+        for (int i = 0; i < member.length; i++) {
+         summ = summ + member[i].getSalary();
+        }
+        System.out.println("Общие затраты на зарплату: " + summ);
+        System.out.println();
+
+        summ = summ / member.length;
+        System.out.println("Средняя зарплата в компании: " + summ);
+        System.out.println();
+
+        double max = 0;
+        for (int i = 0; i < member.length; i++) {
+            if (max < member[i].getSalary()) {
+                max = member[i].getSalary();
+            }
+        }
+        System.out.println("Максимальная зарплата в компании: " + max);
+        System.out.println();
+
+        double min = 1000000;
+        for (int i = 0; i < member.length; i++) {
+            if (min > member[i].getSalary()) {
+                min = member[i].getSalary();
+            }
+        }
+        System.out.println("Минимальная зарплата в компании: " + min);
+        System.out.println();
+
+        for (int i = 0; i < member.length; i++) {
+            System.out.println(member[i].getFirstName() + " " + member[i].getSecondName() + " " + member[i].getThirdName());
+        }
+
+
     }
 }
